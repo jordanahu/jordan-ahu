374aac283d9102305db634068176d9df76465d8f
jordanahu <ahujordan@yahoo.com>
2023-01-19 17:08:40 +0000

    updated with education

--- src/components/navbar/useStyles.js
+++ src/components/navbar/useStyles.js
@@ -1,26 +1,24 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-
-   root:{
-     width:"100%",
-     overflowX:"hidden",
-     position:"absolute",
-     top:"0",
-     zIndex:"100"
-   },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    toolbar:{
-      display:"flex",
-      justifyContent:"space-between",
-      backgroundColor:theme.palette.background.paper,
-      
-    }
-  }));
+  root: {
+    width: "100%",
+    overflowX: "hidden",
+    position: "absolute",
+    top: "0",
+    zIndex: "100",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
   export default useStyles;
